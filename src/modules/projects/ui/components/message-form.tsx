4b03cbaee60293e55df0c106dfc0b7280c387702
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-07-02 19:28:01 +0530

    complete the message ui

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -1,47 +1,127 @@
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { useState } from "react"
-import { toast } from "sonner"
-import { zodResolver } from "@hookform/resolvers/zod"
-import TextareaAutosize from "react-textarea-autosize"
-import { ArrowUpIcon, Loader2Icon } from "lucide-react"
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { cn } from "@/lib/utils"
-import { useTRPC } from "@/trpic/client"
-import { Button } from "@/components/ui/button"
-import { Form, FormField } from "@/components/ui/form"
-import { TypeOf } from "zod"
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useState } from "react";
+import { toast } from "sonner";
+import { zodResolver } from "@hookform/resolvers/zod";
+import TextareaAutosize from "react-textarea-autosize";
+import { ArrowUpIcon, Loader2Icon } from "lucide-react";
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { cn } from "@/lib/utils";
+import { useTRPC } from "@/trpic/client";
+import { Button } from "@/components/ui/button";
+import { Form, FormField } from "@/components/ui/form";
+import { TypeOf } from "zod";
 
 interface Props {
-    projectId: string
+  projectId: string;
 }
 
 const formSchema = z.object({
-    value: z.string()
-        .min(1, { message: "Message is required" })
-        .max(1000, { message: "Message must be less than 1000 characters" }),
-})
-
+  value: z
+    .string()
+    .min(1, { message: "Message is required" })
+    .max(1000, { message: "Message must be less than 1000 characters" }),
+});
 
 export const MessageForm = ({ projectId }: Props) => {
-    
-    const form = useForm<z.infer<typeof formSchema>> ({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            value: "",
-        },
+  const [isFocused, setIsFocused] = useState(false);
+  const showUser = false;
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      value: "",
+    },
+  });
+
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
+      onSuccess: () => {
+        form.reset();
+        queryClient.invalidateQueries(
+          trpc.messages.getMany.queryOptions({ projectId })
+        );
+        // TODO: invlidate usage status
+      },
+      onError: (error) => {
+        toast.error(
+            //TODO: redirect to pricing page if specific error
+          error.message || "An error occurred while creating the message"
+        );
+      },
     })
+  );
 
-    const onSubmit = (value: z.infer<typeof formSchema>) => {
-        console.log("Submitted value:", value);
-    }
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    await createMessage.mutateAsync({
+      value: value.value,
+      projectId,
+    });
+  };
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handlerSubmit(onSubmit)} className={cn ("relative border p-4 rounded-xl bg-sidebar transition-all")}>
-                
-            </form>
-            
-        </Form>
-    )
-}
+  const isPending = createMessage.isPending;
+  const isButtonDisabled = isPending || !form.formState.isValid;
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn(
+          "relative border p-4 rounded-xl bg-sidebar transition-all",
+          isFocused && "shadow-xs",
+          showUser && "rounded-t-none"
+        )}
+      >
+        <FormField
+          control={form.control}
+          name="value"
+          render={({ field }) => (
+            <TextareaAutosize
+              {...field}
+              disabled={isPending}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              minRows={2}
+              maxRows={8}
+              className="pt-4 resize-none border-none w-full outline-none bg-transparent"
+              placeholder="What would like to build"
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+                  e.preventDefault();
+                  form.handleSubmit(onSubmit)(e);
+                }
+              }}
+            />
+          )}
+        />
+        <div className="flex gap-x-2 items-end justify-between pt-2">
+          <div className="text-[10px] text-muted-foreground font-mono">
+            <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono font-medium text-muted-foreground">
+              <span>&#8984;</span>Enter
+            </kbd>
+            &nbsp;to submit
+          </div>
+          <Button
+            disabled={isButtonDisabled}
+            className={cn(
+              "size-8 rounded-full",
+              isButtonDisabled && "bg-muted-foreground border"
+            )}
+          >
+            {isPending ? (
+              <Loader2Icon className="size-4 animate-spin" />
+            ) : (
+              <ArrowUpIcon />
+            )}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
