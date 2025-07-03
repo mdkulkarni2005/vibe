f96f27424f768954350c116cff956ffb94845513
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-07-03 19:47:15 +0530

    Add FragmentWeb component for fragment preview
    Introduces the FragmentWeb component to display a web preview of a fragment using an iframe, with controls to refresh, copy, and open the sandbox URL. Integrates this component into ProjectView to replace the preview placeholder when an active fragment is present.

---
+++ src/modules/projects/ui/components/fragment-web.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { ExternalLinkIcon, RefreshCcwIcon } from "lucide-react";
+import { Fragment } from "@/generated/prisma";
+import { useState } from "react";
+
+interface Props {
+  data: Fragment;
+}
+
+export function FragmentWeb({ data }: Props) {
+  const [fragmentKey, setFragmentKey] = useState(0);
+  const [copied, setCopied] = useState(false);
+
+  const onRefresh = () => {
+    setFragmentKey((prev) => prev + 1);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(data.sandboxUrl);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="p-2 border-b bg-sidebar flex items-center gap-x-2">
+        <Button size="sm" variant="outline" onClick={onRefresh}>
+          <RefreshCcwIcon />
+        </Button>
+        <Button
+          size="sm"
+          variant="outline"
+          onClick={handleCopy}
+          disabled={!data.sandboxUrl || copied}
+          className="flex-1 justify-start text-start font-normal"
+        >
+          <span className="truncate">{data.sandboxUrl}</span>
+        </Button>
+        <Button
+          size="sm"
+          disabled={!data.sandboxUrl}
+          variant="outline"
+          onClick={() => {
+            if (data.sandboxUrl) return window.open(data.sandboxUrl, "_blank");
+          }}
+        >
+          <ExternalLinkIcon />
+        </Button>
+      </div>
+      <iframe
+        key={fragmentKey}
+        className="w-full h-full"
+        sandbox="allow-forms allow-scripts allow-same-origin"
+        loading="lazy"
+        src={data.sandboxUrl}
+      ></iframe>
+    </div>
+  );
+}
