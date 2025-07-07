3c946093e18c8c1682fa47eca821fcd404af19e0
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-07-07 19:03:26 +0530

    Show upgrade button only for non-Pro users
    Added logic to check for Pro plan access using Clerk's useAuth hook. The 'Upgrade' button and 'free credits' label are now only shown to users without Pro access in both usage and project view components.

--- src/modules/projects/ui/components/usage.tsx
+++ src/modules/projects/ui/components/usage.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { CrownIcon } from "lucide-react";
 import { formatDuration, intervalToDuration } from "date-fns";
 import { Button } from "@/components/ui/button";
+import { useAuth } from "@clerk/nextjs";
 
 interface Props {
   points: number;
@@ -9,11 +10,13 @@ interface Props {
 }
 
 export const Usage = ({ points, msBeforNext }: Props) => {
+  const { has } = useAuth();
+  const hasProAccess = has?.({ plan: "pro" });
   return (
     <div className="rounded-t-xl bg-background border border-b-0 p-2.5">
       <div className="flex items-center gap-x-2">
         <div>
-          <p className="text-sm">{points} free credits remaining</p>
+          <p className="text-sm">{points} {hasProAccess ? "": "free"} credits remaining</p>
           <p className="text-xs text-muted-foreground">
             Resets in{" "}
             {formatDuration(
@@ -25,11 +28,13 @@ export const Usage = ({ points, msBeforNext }: Props) => {
             )}
           </p>
         </div>
+        {!hasProAccess && (
         <Button asChild size="sm" variant="tertiary" className="ml-auto">
           <Link href="/pricing">
             <CrownIcon /> Upgrade
           </Link>
         </Button>
+        )}
       </div>
     </div>
   );
